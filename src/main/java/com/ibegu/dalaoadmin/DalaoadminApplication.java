82606109c918fff4191b1cbbc9f66d6db8d39e0d
MrBlueWhale <15775837993@163.com>
2021-04-09 11:21:27 +0800

    1-1 git init

---
+++ src/main/java/com/ibegu/dalaoadmin/DalaoadminApplication.java
@@ -0,0 +1,13 @@
+package com.ibegu.dalaoadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DalaoadminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DalaoadminApplication.class, args);
+    }
+
+}
