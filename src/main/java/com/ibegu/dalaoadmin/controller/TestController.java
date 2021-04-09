e54601a357761cc980d7db612592c01ada4c3c31
MrBlueWhale <15775837993@163.com>
2021-04-09 17:38:57 +0800

    1-3 增加 自定义配置项的示例

--- src/main/java/com/ibegu/dalaoadmin/controller/TestController.java
+++ src/main/java/com/ibegu/dalaoadmin/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.ibegu.dalaoadmin.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,11 +14,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
+    @Value("${test.hello:TEST}")
+    private String testHello;
+
 
     @GetMapping("/hello")
     public String hello(){
 
-        return "Hello World !";
+        return "Hello World !" + testHello;
 
     }
 
