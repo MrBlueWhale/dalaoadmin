3ac498cf4ca365a4ab81bcabf485456b7ab920fb
MrBlueWhale <15775837993@163.com>
2021-04-23 10:39:00 +0800

    1-13 封装请求参数和返回参数

--- src/main/java/com/ibegu/dalaoadmin/controller/DemoController.java
+++ src/main/java/com/ibegu/dalaoadmin/controller/DemoController.java
@@ -2,7 +2,9 @@ package com.ibegu.dalaoadmin.controller;
 
 import com.ibegu.dalaoadmin.domain.Demo;
 import com.ibegu.dalaoadmin.domain.Test;
+import com.ibegu.dalaoadmin.req.DemoReq;
 import com.ibegu.dalaoadmin.resp.CommonResp;
+import com.ibegu.dalaoadmin.resp.DemoResp;
 import com.ibegu.dalaoadmin.service.DemoService;
 import com.ibegu.dalaoadmin.service.TestService;
 import org.springframework.beans.factory.annotation.Value;
@@ -30,10 +32,21 @@ public class DemoController {
 
 
     @GetMapping("/list")
-    public CommonResp list(){
+    //Controller层不出现domain实体类
 
-        CommonResp<List<Demo>> resp = new CommonResp<>();
-        List<Demo> list = demoService.list();
+    // public CommonResp list(){
+    //
+    //     CommonResp<List<Demo>> resp = new CommonResp<>();
+    //     List<Demo> list = demoService.list();
+    //     resp.setContent(list);
+    //     return resp;
+    //
+    // }
+
+    public CommonResp list(DemoReq demoReq){
+
+        CommonResp<List<DemoResp>> resp = new CommonResp<>();
+        List<DemoResp> list = demoService.list(demoReq);
         resp.setContent(list);
         return resp;
 
--- src/main/java/com/ibegu/dalaoadmin/service/DemoService.java
+++ src/main/java/com/ibegu/dalaoadmin/service/DemoService.java
@@ -1,12 +1,17 @@
 package com.ibegu.dalaoadmin.service;
 
 import com.ibegu.dalaoadmin.domain.Demo;
+import com.ibegu.dalaoadmin.domain.DemoExample;
 import com.ibegu.dalaoadmin.domain.Test;
 import com.ibegu.dalaoadmin.mapper.DemoMapper;
 import com.ibegu.dalaoadmin.mapper.TestMapper;
+import com.ibegu.dalaoadmin.req.DemoReq;
+import com.ibegu.dalaoadmin.resp.DemoResp;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -28,6 +33,32 @@ public class DemoService {
         return demoMapper.selectByExample(null);
         // return demoMapper.selectByExample(new DemoExample());
     }
+
+    public List<DemoResp> list(DemoReq demoReq){
+
+        DemoExample demoExample = new DemoExample();
+        DemoExample.Criteria criteria = demoExample.createCriteria();
+        criteria.andNameLike("%" + demoReq.getName() + "%");
+
+        List<Demo> demoList = demoMapper.selectByExample(demoExample);
+
+
+        //利用循环从将demo封装为demoresp
+        List<DemoResp> demoRespList = new ArrayList<>();
+        for (Demo demo : demoList) {
+            DemoResp demoResp = new DemoResp();
+            BeanUtils.copyProperties(demo, demoResp);
+            // demoResp.setId(123L);
+            demoRespList.add(demoResp);
+        }
+
+        return demoRespList;
+
+
+        // return demoMapper.selectByExample(new DemoExample());
+    }
+
+
 
 
 
