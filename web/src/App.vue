804001fd41d40e663e51dbf994995c45fbab6b31
MrBlueWhale <15775837993@163.com>
2021-05-13 22:19:48 +0800

    2-4 制作admin-header组件

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,24 +1,28 @@
 <template>
   <a-layout>
-    <a-layout-header class="header">
-      <div class="logo" />
-      <a-menu
-          theme="dark"
-          mode="horizontal"
-          v-model:selectedKeys="selectedKeys1"
-          :style="{ lineHeight: '64px' }"
-      >
-        <a-menu-item key="1">nav 1</a-menu-item>
-        <a-menu-item key="2">nav 2</a-menu-item>
-        <a-menu-item key="3">nav 3</a-menu-item>
-      </a-menu>
-    </a-layout-header>
+<admin-header></admin-header>
     <router-view></router-view>
     <a-layout-footer style="text-align: center">
       Dalao Template @2021
     </a-layout-footer>
   </a-layout>
 </template>
+
+<script lang="ts">
+import {defineComponent} from 'vue';
+import AdminHeader from '@/components/admin-header.vue';
+
+export default defineComponent({
+  name: 'app',
+  components: {
+    AdminHeader,
+  },
+
+});
+</script>
+
+
+
 
 <style>
 #components-layout-demo-top-side-2 .logo {
